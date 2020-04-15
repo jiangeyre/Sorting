50b3aa6d2e5f25dfe4457217b1b45a82823a2759
Lexie Jiang <lexiemingjiang@gmail.com>
2020-04-15 08:40:25 -0700

    Commit 2: recursive_sorting.py complete, test_ran=OK, MVP Day 2 met

--- src/recursive_sorting/quicksort.py
+++ src/recursive_sorting/quicksort.py
@@ -15,3 +15,17 @@ def quick_sort(arr):
     # Sort smaller and larger
     # Concatenate smaller + pivot + larger
     return quick_sort(smaller) + [pivot] + quick_sort(larger)
+
+
+# MERGESORT
+[2, 0, 1, 3, 6, 9, 8, 5, 7, 4]
+# (base case) If the array is empty or length 1, return
+# Split the arrays into half
+arr1 = [2, 0, 1, 3, 6]
+arr2 = [9, 8, 5, 7, 4]
+# Sort each half
+arr1 = [0,1,2,3,6]
+arr2 = [4,5,7,8,9]
+# Merge them back together
+# Compare the first values of each array, add smaller of the 2 to result
+merged = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,15 +1,45 @@
-# TO-DO: complete the helpe function below to merge 2 sorted arrays
+# TO-DO: complete the helper function below to merge 2 sorted arrays
 def merge( arrA, arrB ):
     elements = len( arrA ) + len( arrB )
     merged_arr = [0] * elements
     # TO-DO
-    
+    # index iterations for each arr
+    a, b, c = 0, 0, 0
+    # compare and sort the elements of both of the arrays to a merged array
+    while a < len(arrA) and b < len(arrB):
+        if arrA[a] < arrB[b]:
+            merged_arr[c] = arrA[a]
+            c = c + 1
+            a = a + 1
+        else:
+            merged_arr[c] = arrB[b]
+            c = c + 1
+            b = b + 1
+    # remaining elements need to be added to the merged arr
+    while a < len(arrA):
+        merged_arr[c] = arrA[a]
+        c = c + 1
+        a = a + 1
+    while b < len(arrB):
+        merged_arr[c] = arrB[b]
+        c = c + 1
+        b = b + 1
+
     return merged_arr
 
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort( arr ):
     # TO-DO
+    # establish a base case
+    if len(arr) <= 1:
+        return arr
+    # divide until single element arrays
+    mid = len(arr) // 2
+    l = merge_sort(arr[mid:])
+    r = merge_sort(arr[:mid])
+    # compare values and sort the arrays
+    arr = merge(l, r)
 
     return arr
 
